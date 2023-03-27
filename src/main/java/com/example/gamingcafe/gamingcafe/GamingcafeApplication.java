c30b443f6ac00129e35cb4669e5c6bb52346de13
Santhil Kalantre <santhil@Santhils-MacBook-Air.local>
2023-03-27 19:24:53 +0530

    first commit

---
+++ src/main/java/com/example/gamingcafe/gamingcafe/GamingcafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamingcafe.gamingcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingcafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamingcafe/gamingcafe/GamingcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamingcafe.gamingcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
